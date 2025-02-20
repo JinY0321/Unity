31465ffcf8eb84f884b57f0db70fd83cd71741ce
JinY0321 <sunplant0321@gmail.com>
2025-02-20 17:23:24 +0900

    fix : 플레이어 회전버그 수정

--- Metaverse_Project/Assets/Script/Entity/PlayerController.cs
+++ Metaverse_Project/Assets/Script/Entity/PlayerController.cs
@@ -15,20 +15,13 @@ public class PlayerController : BaseController
     protected override void HandleAction()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertial = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2(horizontal, vertial).normalized;
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
 
-        Vector2 mousePosition = Input.mousePosition;
-        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
-        lookDirection = (worldPos - (Vector2)transform.position);
-
-        if (lookDirection.magnitude < .9f)
+        // 이동 입력이 있을 때만 방향을 변경
+        if (movementDirection != Vector2.zero)
         {
-            lookDirection = Vector2.zero;
-        }
-        else
-        {
-            lookDirection = lookDirection.normalized;
+            lookDirection = movementDirection; // 키보드 방향을 바라보게 설정
         }
     }
 }
